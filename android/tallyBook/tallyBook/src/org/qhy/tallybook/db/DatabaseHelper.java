aa06ab88737ec89312603954bea7b3edc4d6e110
U-qihuiyong-PC\qihuiyong <qihuiyong@qihuiyong-PC.(none)>
2013-06-06 11:26:05 +0800

    s

--- android/tallyBook/tallyBook/src/org/qhy/tallybook/db/DatabaseHelper.java
+++ android/tallyBook/tallyBook/src/org/qhy/tallybook/db/DatabaseHelper.java
@@ -4,24 +4,27 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
-
+/**
+ * 数据库操作帮助类
+ * @author qihuiyong
+ *
+ */
 public class DatabaseHelper extends SQLiteOpenHelper {
-	private static final String TAG = "DatabaseHelper";
+	private static final String TAG = DatabaseHelper.class.getSimpleName();
 	
 	public DatabaseHelper(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		//创建类型表
+		db.execSQL(TallyBookDatabase.CREATE_TABLE_TYPE_SQL);
+		//创建记录表
+		db.execSQL(TallyBookDatabase.CREATE_TABLE_RECORD_SQL);
 	}
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
--- android/tallyBook/tallyBook/src/org/qhy/tallybook/db/TallyBookDatabase.java
+++ android/tallyBook/tallyBook/src/org/qhy/tallybook/db/TallyBookDatabase.java
@@ -1,8 +1,12 @@
 package org.qhy.tallybook.db;
 
 public class TallyBookDatabase {
-	private static final String TAG = "TallyBookDatabase";
+	/**/
 	public static final String DATABASE_NAME = "qhy_tallyBook";
+	public static final int DATABASE_VERSION = 1;
 	public static final String TYPE_TALLYBOOK_TABLE = "tallyType";
 	public static final String RECORED_TALLYBOOK_TABLE = "consumeRecord";
+	/*create Table sql*/
+	public static final String CREATE_TABLE_TYPE_SQL = "";
+	public static final String CREATE_TABLE_RECORD_SQL = "";
 }
