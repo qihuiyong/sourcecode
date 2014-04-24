d46479aa3142f1a81a73453a19669c51a72ebbae
U-AILK\qihy <qihy@qihy.ailk.com>
2014-04-24 09:11:59 +0800

    update invokeBaiduPOI

--- java/BaiduPoiSerach/src/org/qhy/test/TestInvokeBaiduLogin.java
+++ java/BaiduPoiSerach/src/org/qhy/test/TestInvokeBaiduLogin.java
@@ -14,86 +14,69 @@ import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 
 public class TestInvokeBaiduLogin {
 
 	public static void main(String[] args) throws HttpException, IOException {
+		// 创建httpClient客户端
 		HttpClient client = new HttpClient();
+		// 百度的登陆url
 		String loginUrl = "https://passport.baidu.com/v2/api/?login";
-		String tokenUrl = "https://passport.baidu.com/v2/api/?getapi&class=login&tpl=mn&tangram=true";
-		// String uNameDomName = "userName", uPasswordDomName = "password";
-		// 设置代理服务器地址和端口
-		client.getHostConfiguration().setProxy("10.1.251.240", 808);
+		// 设置代理
+		String httpProxyHost = "10.1.251.240";
+		int proxyPort = 808;
+		client.getHostConfiguration().setProxy(httpProxyHost, proxyPort);
+		// 代理验证信息
 		client.getParams().setAuthenticationPreemptive(true);
 		UsernamePasswordCredentials creds = new UsernamePasswordCredentials(
 				"pdd", "pdd");
 		client.getState().setProxyCredentials(AuthScope.ANY, creds);
+		// 代理设置结束(如果不需要代理这段可以注释掉)
 		// 登陆百度
 		PostMethod psotMethod = new PostMethod(loginUrl);
-//		// 获取token
-//		HttpMethod tokenGet = new GetMethod(tokenUrl);
-//		client.executeMethod(tokenGet);
-//		BufferedReader br = new BufferedReader(new InputStreamReader(
-//				tokenGet.getResponseBodyAsStream(), "UTF-8"));
-//		String line = "";
-//		while ((line = br.readLine()) != null) {
-//			if (line.contains("login_token")) {
-//				line = line.substring(line.indexOf("='") + 2,
-//						line.indexOf("';"));
-//				break;
-//			}
-//		}
-
-		// params2.put("token", r.getToken());
-
-		// 设置登陆时要求的信息，一般就用户名和密码，验证码自己处理了
+		// 设置登陆参数
 		NameValuePair[] data = {
 				new NameValuePair("class", "login"),
 				new NameValuePair("tpl", "mn"),
 				new NameValuePair("tangram", "true"),
 				new NameValuePair("isPhone", "false"),
 				new NameValuePair("loginType", "1"),
-				new NameValuePair("verifycode", ""),
 				new NameValuePair("token", "f6994339731518f4c50d30acc10cbc6f"),
 				new NameValuePair("callback",
 						"parent.bdPass.api.login._postCallback"),
 				new NameValuePair("username", "xxxxx"),
-				new NameValuePair("password", "xxx")};
+				new NameValuePair("password", "xxx") };
 		psotMethod.setRequestBody(data);
-		// 设置 HttpClient 接收 Cookie,用与浏览器一样的策略
+		// HttpClient像浏览器一样接收 Cookie,
 		client.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
 		client.executeMethod(psotMethod);
 		Header[] haeders = psotMethod.getRequestHeaders();
-		// 获得登陆后的 Cookie
+		// 获取cookie
 		Cookie[] cookies = client.getState().getCookies();
-		String tmpcookies = "";
+		String cookiesStr = "";
 		for (Cookie c : cookies) {
-			tmpcookies += c.toString() + ";";
+			cookiesStr += c.toString() + ";";
 		}
 		System.out.println("psotMethod--response:"
 				+ new String(psotMethod.getResponseBody(), "UTF-8"));
-
+		psotMethod.releaseConnection();
 		// 使用 GET 方法 ，如果服务器需要通过 HTTPS 连接，那只需要将下面 URL 中的 http 换成 https
 		HttpMethod method = new GetMethod(
 				"http://api.map.baidu.com/place/v2/search?&q="
 						+ URLEncoder.encode("饭店", "UTF-8") + "&region="
 						+ URLEncoder.encode("北京", "UTF-8")
 						+ "&output=json&ak=1649c026d0cea10ccf5741348526cb44");
-//		 method.setRequestHeader("cookie", tmpcookies);
-//		 method.getParams().setBooleanParameter(HttpMethodParams.SINGLE_COOKIE_HEADER, true);
-		// for (Header header : haeders) {
-		// method.setRequestHeader(header);
-		// }
-		// BAIDUID=49ED0908707E55D43F5DD12FDFA5A2E3:FG=1;
-		// 使用POST方法
-		// HttpMethod method = new PostMethod("http://java.sun.com");
+		method.setRequestHeader("Cookie", cookiesStr);
+		method.getParams().setBooleanParameter(
+				HttpMethodParams.SINGLE_COOKIE_HEADER, true);
 		client.executeMethod(method);
-		// 打印服务器返回的状态
+		// 打印 服务器状态
 		System.out.println("状态：" + method.getStatusLine());
-		// 打印返回的信息
-		System.out.println("返回内容:"
+		// 打印信息
+		System.out.println("内容:"
 				+ new String(method.getResponseBody(), "UTF-8"));
-		// 释放连接
+		// 关闭连接
 		method.releaseConnection();
 	}
 }
